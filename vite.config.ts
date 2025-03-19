487eef4137aaf41cc2d4b0a50bad0ee197405d15
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-03-19 17:29:04 +0000

    added live demo link to the readme file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Digital_Marketing_Agency/",
   plugins: [tailwindcss(), react()],
 });
